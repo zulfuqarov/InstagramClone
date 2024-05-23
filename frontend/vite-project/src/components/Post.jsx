e7791ce6e929ace2b25ada7d48c80c4f1a4d8215
zulfuqarov <zulfuqarov.nebi@gmail.com>
2024-05-23 23:16:14 +0400

    Home userProfile updaiting

--- frontend/vite-project/src/components/Post.jsx
+++ frontend/vite-project/src/components/Post.jsx
@@ -8,30 +8,32 @@ const Post = () => {
     context.getUserPost()
   }, [])
 
-  return (
-    <div className='pt-[100px]'>
-      <div>
-        <p className='text-center text-[22px] border-b-[2px] pb-[5px]'><i className="fa-solid fa-signs-post pr-[10px]"></i>Post</p>
-      </div>
-      <div className='grid grid-cols-3 gap-4 pt-[20px] '>
-        {
-          context.userPost &&
-          context.userPost.map((oneMap, index) => (
-            <div key={index} className='relative group' /*onMouseEnter={() => context.getPostComments(oneMap._id)}*/>
-              <img className='rounded-md w-full h-full' src={`${oneMap.img}`} alt="" />
-              <div className='opacity-0 z-10  dden group-hover:opacity-40 transition-all absolute top-0 left-0 w-full h-full bg-slate-900 	'>
+  if (context.userPost.length > 0) {
+    return (
+      <div className='pt-[100px]'>
+        <div>
+          <p className='text-center text-[22px] border-b-[2px] pb-[5px]'><i className="fa-solid fa-signs-post pr-[10px]"></i>Post</p>
+        </div>
+        <div className='grid grid-cols-3 gap-4 pt-[20px] '>
+          {
+            context.userPost &&
+            context.userPost.map((oneMap, index) => (
+              <div key={index} className='relative group' /*onMouseEnter={() => context.getPostComments(oneMap._id)}*/>
+                <img className='rounded-md w-full h-full' src={`${oneMap.img}`} alt="" />
+                <div className='opacity-0 z-10  dden group-hover:opacity-40 transition-all absolute top-0 left-0 w-full h-full bg-slate-900 	'>
 
+                </div>
+                <div className='opacity-0 group-hover:opacity-100 transition-all   text-white z-20  absolute top-0 left-0 flex justify-center items-center w-full h-full'>
+                  <i className="fa-solid fa-heart "><span className='text-[14px] pl-[7px] inline-block'>{oneMap.likes.length}</span></i>
+                  <i className="fa-solid fa-comment pl-[20px]"><span className='text-[14px] pl-[7px] inline-block'>{oneMap.comments.length}</span></i>
+                </div>
               </div>
-              <div className='opacity-0 group-hover:opacity-100 transition-all   text-white z-20  absolute top-0 left-0 flex justify-center items-center w-full h-full'>
-                <i className="fa-solid fa-heart "><span className='text-[14px] pl-[7px] inline-block'>{oneMap.likes.length}</span></i>
-                <i className="fa-solid fa-comment pl-[20px]"><span className='text-[14px] pl-[7px] inline-block'>{oneMap.comments.length}</span></i>
-              </div>
-            </div>
-          ))
-        }
+            ))
+          }
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Post
--- frontend/vite-project/src/components/UserProfile.jsx
+++ frontend/vite-project/src/components/UserProfile.jsx
@@ -1,111 +1,127 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { ContextInsta } from '../Context/Context'
 
 const UserProfile = () => {
-    return (
-        <div className='pt-[60px] fixed w-[235px]'>
-            <div className='flex items-center justify-between pb-[15px]'>
-                <div className='flex items-center'>
-                    <div >
-                        <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
-                    </div>
-                    <div className='pl-[20px] text-gray-600 text-[15px]'>
-                        <p>zulfuqarov</p>
-                        <small>Azerbaijan Bakue</small>
-                    </div>
-                </div>
-                <div>
-                    <button className='text-[13px] text-blue-500'>Sigin Out</button>
-                </div>
-            </div>
 
-            <div className='pt-[20px] border-t-[1px] border-gray-300'>
-                <div className='flex items-center justify-between w-full'>
-                    <p className='text-[15px] text-gray-400'>Suggestions for you</p>
-                    <p className='text-[15px] text-gray-600 cursor-pointer'>See All</p>
-                </div>
-                <div className='pt-[10px] '>
-                    <div className='flex items-center justify-between'>
+    const context = useContext(ContextInsta)
+
+    useEffect(() => {
+        context.getUserProfile()
+    }, [])
+
+
+    if (context.userProfile) {
+        return (
+            <div className='pt-[60px] fixed w-[235px]'>
+                {
+                    context.userProfile &&
+                    <div className='flex items-center justify-between pb-[15px]'>
                         <div className='flex items-center'>
                             <div >
-                                <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
+                                <img className="h-8 w-8 object-cover  rounded-full" src={`${context.userProfile.profilePicture}`} />
                             </div>
                             <div className='pl-[20px] text-gray-600 text-[15px]'>
-                                <p>zulfuqarov</p>
-                                <small>Azerbaijan Bakue</small>
+                                <p>{context.userProfile.fullName}</p>
+                                <small>{context.userProfile.bio}</small>
                             </div>
                         </div>
                         <div>
-                            <button className='text-[13px] text-blue-500'>Follow</button>
+                            <button className='text-[13px] text-blue-500'>Sigin Out</button>
                         </div>
                     </div>
-                </div>
-                <div className='pt-[10px] '>
-                    <div className='flex items-center justify-between'>
-                        <div className='flex items-center'>
-                            <div >
-                                <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
+                }
+
+
+                <div className='pt-[20px] border-t-[1px] border-gray-300'>
+                    <div className='flex items-center justify-between w-full'>
+                        <p className='text-[15px] text-gray-400'>Suggestions for you</p>
+                        <p className='text-[15px] text-gray-600 cursor-pointer'>See All</p>
+                    </div>
+                    <div className='pt-[10px] '>
+                        <div className='flex items-center justify-between'>
+                            <div className='flex items-center'>
+                                <div >
+                                    <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
+                                </div>
+                                <div className='pl-[20px] text-gray-600 text-[15px]'>
+                                    <p>zulfuqarov</p>
+                                    <small>Azerbaijan Bakue</small>
+                                </div>
                             </div>
-                            <div className='pl-[20px] text-gray-600 text-[15px]'>
-                                <p>zulfuqarov</p>
-                                <small>Azerbaijan Bakue</small>
+                            <div>
+                                <button className='text-[13px] text-blue-500'>Follow</button>
                             </div>
-                        </div>
-                        <div>
-                            <button className='text-[13px] text-blue-500'>Follow</button>
                         </div>
                     </div>
-                </div>
-                <div className='pt-[10px] '>
-                    <div className='flex items-center justify-between'>
-                        <div className='flex items-center'>
-                            <div >
-                                <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
+                    <div className='pt-[10px] '>
+                        <div className='flex items-center justify-between'>
+                            <div className='flex items-center'>
+                                <div >
+                                    <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
+                                </div>
+                                <div className='pl-[20px] text-gray-600 text-[15px]'>
+                                    <p>zulfuqarov</p>
+                                    <small>Azerbaijan Bakue</small>
+                                </div>
                             </div>
-                            <div className='pl-[20px] text-gray-600 text-[15px]'>
-                                <p>zulfuqarov</p>
-                                <small>Azerbaijan Bakue</small>
+                            <div>
+                                <button className='text-[13px] text-blue-500'>Follow</button>
                             </div>
-                        </div>
-                        <div>
-                            <button className='text-[13px] text-blue-500'>Follow</button>
                         </div>
                     </div>
-                </div>
-                <div className='pt-[10px] '>
-                    <div className='flex items-center justify-between'>
-                        <div className='flex items-center'>
-                            <div >
-                                <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
+                    <div className='pt-[10px] '>
+                        <div className='flex items-center justify-between'>
+                            <div className='flex items-center'>
+                                <div >
+                                    <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
+                                </div>
+                                <div className='pl-[20px] text-gray-600 text-[15px]'>
+                                    <p>zulfuqarov</p>
+                                    <small>Azerbaijan Bakue</small>
+                                </div>
                             </div>
-                            <div className='pl-[20px] text-gray-600 text-[15px]'>
-                                <p>zulfuqarov</p>
-                                <small>Azerbaijan Bakue</small>
+                            <div>
+                                <button className='text-[13px] text-blue-500'>Follow</button>
                             </div>
-                        </div>
-                        <div>
-                            <button className='text-[13px] text-blue-500'>Follow</button>
                         </div>
                     </div>
-                </div>
-                <div className='pt-[10px] '>
-                    <div className='flex items-center justify-between'>
-                        <div className='flex items-center'>
-                            <div >
-                                <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
+                    <div className='pt-[10px] '>
+                        <div className='flex items-center justify-between'>
+                            <div className='flex items-center'>
+                                <div >
+                                    <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
+                                </div>
+                                <div className='pl-[20px] text-gray-600 text-[15px]'>
+                                    <p>zulfuqarov</p>
+                                    <small>Azerbaijan Bakue</small>
+                                </div>
                             </div>
-                            <div className='pl-[20px] text-gray-600 text-[15px]'>
-                                <p>zulfuqarov</p>
-                                <small>Azerbaijan Bakue</small>
+                            <div>
+                                <button className='text-[13px] text-blue-500'>Follow</button>
                             </div>
                         </div>
-                        <div>
-                            <button className='text-[13px] text-blue-500'>Follow</button>
+                    </div>
+                    <div className='pt-[10px] '>
+                        <div className='flex items-center justify-between'>
+                            <div className='flex items-center'>
+                                <div >
+                                    <img className="h-8 w-8  rounded-full" src="https://picsum.photos/id/1027/150/150" />
+                                </div>
+                                <div className='pl-[20px] text-gray-600 text-[15px]'>
+                                    <p>zulfuqarov</p>
+                                    <small>Azerbaijan Bakue</small>
+                                </div>
+                            </div>
+                            <div>
+                                <button className='text-[13px] text-blue-500'>Follow</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
 
 export default UserProfile
